130e1fcb49c3641a4b423ff1cee297678d4c9936
jetchatbotai <yusufeymenyagmur381@gmail.com>
2026-03-27 19:59:27 +0300

    Update AlarmStopReceiver.java

--- native-android/java/com/timertrink/app/AlarmStopReceiver.java
+++ native-android/java/com/timertrink/app/AlarmStopReceiver.java
@@ -1,1 +1,13 @@
+package com.timertrink.app;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmStopReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent stopIntent = new Intent(context, AlarmSoundService.class);
+        context.stopService(stopIntent);
+    }
+}
