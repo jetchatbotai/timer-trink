05f1a2c61283009249ed94d2df037e48a3f9085c
jetchatbotai <yusufeymenyagmur381@gmail.com>
2026-03-27 19:58:12 +0300

    Update AlarmReceiver.java

--- native-android/java/com/timertrink/app/AlarmReceiver.java
+++ native-android/java/com/timertrink/app/AlarmReceiver.java
@@ -1,1 +1,24 @@
+package com.timertrink.app;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent serviceIntent = new Intent(context, AlarmSoundService.class);
+
+        if (intent != null) {
+            serviceIntent.putExtra("title", intent.getStringExtra("title"));
+            serviceIntent.putExtra("message", intent.getStringExtra("message"));
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(serviceIntent);
+        } else {
+            context.startService(serviceIntent);
+        }
+    }
+}
